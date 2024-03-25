21e76d591adb047ef1e3d8cfcd7c71ab6d17497b
Jian Jang <pureunnabi@gmail.com>
2024-03-25 11:08:02 +0900

    playground/hello/ => playground/ -> hello/, playground/hello_html/ => playground/ -> hello_html/

--- README.md
+++ README.md
@@ -20,6 +20,10 @@
    2. urls 
       1. _/playground/hello/_ -> _say_hello()_
       2. _playground/hello_html/_ -> _say_hello_html()_
+4. helloidol/ 
+    1. urls, playground/urls
+       1. _playground/_ -> _hello/_ -> _say_hello()_
+       2. _playground/_ -> _hello_html/_ -> _say_hello_html()_
 
 
 
--- helloidol/urls.py
+++ helloidol/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import playground.views
 
 urlpatterns = [
-    path('playground/hello/', playground.views.say_hello, name='playground_hello'),
-    path('playground/hello_html/', playground.views.say_hello_html, name='playground_hello_html'),
+    path('playground/', include('playground.urls')),  # 수정된 부분
+    # () 없어야댐 say_hello() XXXXXXXXXX
+    # path('playground/hello/', playground.views.say_hello, name='playground_hello'),
+    # path('playground/hello_html/', playground.views.say_hello_html, name='playground_hello_html'),
     path('admin/', admin.site.urls),
 ]
+
