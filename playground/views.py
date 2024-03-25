17f37a5ae47e30d4f270ad992a97c33ea5b8602f
Jian Jang <pureunnabi@gmail.com>
2024-03-25 12:16:46 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,11 @@ def say_hello(request):
     return HttpResponse("Hello World!")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html',{'name' : '지우야', 'greeting' : '안녀엉~'})
 
 def say_bye_html(request):
-    return render(request, 'playground/bye.html')
+    context = {
+        'singer' : '포맨',
+        'title' : '눈 떠보니 이별이더라',
+    }
+    return render(request, 'playground/bye.html', context=context)
