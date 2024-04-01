c6da004138b2c621fc4ff72d2fa2ec46f498f063
Jian Jang <pureunnabi@gmail.com>
2024-04-01 11:51:42 +0900

    urls, views 티파이브/ -> <member>/ -> show_member(member) {context -> }-> member.html

--- 티파이브/urls.py
+++ 티파이브/urls.py
@@ -5,7 +5,7 @@ from 티파이브 import views
 app_name = '티파이브'
 
 urlpatterns = [
-    path('<member>', views.show_member, name='member'),
+    path('<member>/', views.show_member, name='member'),
     # path('jihoon/', views.show_jihoon, name='jihoon'),
     # path('junkyu/', views.show_junkyu, name='junkyu'),
 ]
